c9a0161f337a429f3750d5807ff46c4871f5fe8a
Chris Gunawardena <chris@gunawardena.id.au>
2014-03-06 06:14:08 +0000

    Added scale param

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+<?
+  $scale = 1;
+  if(isset($_GET['scale']))
+    $scale = $_GET['scale'];
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
@@ -8,7 +13,10 @@
 	<style type="text/css">
 		div{ width: 100%; position: relative;  }
 		div img{ width: 100%; }
-		iframe{ border: none; position: absolute;top: 14.3%;left: 7.1%;width: 86.9%;height: 71.4%;}
+		iframe{ border: none; position: absolute;top: 14.3%;left: 7.1%;width: <?=(1/$scale)*86.9?>%;height: <?=(1/$scale)*71.4?>%;
+			transform-origin: 0 0; -ms-transform-origin: 0 0; -webkit-transform-origin: 0 0;
+			transform: scale(<?=$scale?>); -ms-transform: scale(<?=$scale?>); -webkit-transform: scale(<?=$scale?>);
+		}
 	</style>
 </head>
 <body>
